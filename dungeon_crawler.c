1e1f3f2964744c3feddac039a2199f516c1ee4b1
Guikae06 <kaelig.guillemette@gmail.com>
2025-05-20 13:15:27 +0200

    Verschillende typedef ingevuld met mogelijke variables.

--- dungeon_crawler.c
+++ dungeon_crawler.c
@@ -4,22 +4,29 @@
 #include <string.h> // Include this header for strcpy
 
 typedef struct Room{
-    int room_id;
-    
+    	int room_id;
+    	
 } Room;
 
 typedef struct Items{ // structuur voor alle items in de Dungeon spel.
-    char naam[];
-    
-}
+    	char naam[20];
+    	int HealtPotion;
+	int StaminaPotion;
+	
+} Items;
 
 typedef struct Monsters{
-    char naam[];
-}
+    	char naam[15];
+	int healt;
+	int damage;
+	int stamina;
+} Monster;
 
 typedef struct Speeler{ // structuur voor de speler 
-
-}
+	int healt;
+	int damage;
+	int stamina;
+} Player;
 
 int main(){ //de main.
 

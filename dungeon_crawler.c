6dc90c269f761740eb991ed68d36e65571ee7181
Guikae06 <kaelig.guillemette@gmail.com>
2025-05-20 13:09:44 +0200

    Verschillende typedef gemaakt.

--- dungeon_crawler.c
+++ dungeon_crawler.c
@@ -3,3 +3,27 @@
 #include <time.h>
 #include <string.h> // Include this header for strcpy
 
+typedef struct Room{
+    int room_id;
+    
+} Room;
+
+typedef struct Items{ // structuur voor alle items in de Dungeon spel.
+    char naam[];
+    
+}
+
+typedef struct Monsters{
+    char naam[];
+}
+
+typedef struct Speeler{ // structuur voor de speler 
+
+}
+
+int main(){ //de main.
+
+}
+
+
+

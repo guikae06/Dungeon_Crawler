accbe14f68771a1ff462e0b4c88749a2bc4c1087
Guikae06 <kaelig.guillemette@gmail.com>
2025-05-22 11:37:52 +0200

    overgegaan naar een grid dungeon.

--- dungeon_crawler.c
+++ dungeon_crawler.c
@@ -7,6 +7,10 @@
 
 typedef struct Room{
 	int room_id;
+	int x, y; // x en y coördinaten van de kamer.
+	int visited; // of de kamer al bezocht is.
+	int distance; // afstand van de kamer tot de start kamer.
+	int doorCount; // aantal deuren in de kamer.
     Monster *monster;
 	Items *Item;
 	struct Room *doors[MaxDeuren]; //deuren naar andere kamers.
@@ -34,19 +38,28 @@ typedef struct Speeler{ // structuur voor de speler.
 Room *maakRooms(int room_id); //de functie die de kamers maakt.
 void Roomconnection(Room *r_nu, Room *r_volgende); //de functie die de kamers met elkaar verbind.
 void MaakDungeon(Room** rooms, int numRooms); //de functie die de dungeon maakt.
+int roomIdTeller = 0; // teller voor de kamer id's.
 
 int main(){ //de main.
 
 }
 
-Room *maakRooms(int room_id){
+Room *maakRooms(int x_as,int y_as){
+	
 	Room *room = (Room*)malloc(sizeof(Room)); // maak een nieuwe kamer aan.
-	room -> room_id = room_id; // geef de kamer een id.
-	room -> monster = NULL; // er is nog geen monster in de kamer.
-	room -> Item = NULL; // er is nog geen item in de kamer.
+	room -> room_id = roomIdTeller++; // geef de kamer een id.
+	room -> x = x_as; // geef de kamer een x coördinaat.
+	room -> y = y_as; // geef de kamer een y coördinaat.
+	room -> visited = 0; // de kamer is nog niet bezocht.
+	room -> distance = -1; // de afstand is nog niet bekend.
+	room -> doorCount = 0; // er zijn nog geen deuren in de kamer.
+
 	for (int i = 0; i < 4; i++){
 		room -> doors[i] = NULL; // maak de deuren leeg.
 	}
+	room -> monster = NULL; // er is nog geen monster in de kamer.
+	room -> Item = NULL; // er is nog geen item in de kamer.
+
 	return room; // geef de kamer terug.
 }
 
@@ -72,6 +85,15 @@ void MaakDungeon(Room** rooms, int numRooms) {
 	}
 	for (int i = 0; i < numRooms - 1; i++) {
 		connectRooms(rooms[i], rooms[i + 1]);
+	}
+}
+
+void InfoRoom_printen(Room *room){
+	if (room -> room_id == 0){
+		printf("You are int the start room.\n");
+	}
+	else{
+		printf("You are in room %d.\n", room -> room_id);
 	}
 }
 
